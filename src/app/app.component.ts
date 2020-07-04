bef3ed18a460039517446d27800c7af9df75402e
kartik8460 <umeshgupta700@gmail.com>
2020-07-04 12:41:40 +0530

    Word Generator Completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import arrayWords from "../utils/words";
+import arrayCountries from "../utils/country";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+  words: string = '';
+  limit = 10;
+  random: number;
+  handleSlideChange = (newLimit: number) => {
+    this.limit = newLimit;
+  }
+
+  generate = () => {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
+
+  randomCountry = () => {
+    this.random = Math.floor(Math.random()*(arrayCountries.length-0+1)+0);
+    this.words = arrayCountries[this.random];
+  }
 }
